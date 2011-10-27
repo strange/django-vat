28c8dfcc61c8a161deba8e663b336388f790715f
Gustaf Sjöberg <gs@distrop.com>
2011-10-27 20:10:16 +0200

    no eggs!

--- setup.py
+++ setup.py
@@ -3,4 +3,4 @@ from distutils.core import setup
 # maybe add django and suds as required packages.
 
 setup(name='django-vat', version='0.1', author='Gustaf Sjoberg',
-      packages=['vat'])
+      packages=['vat'], zip_safe=False)
